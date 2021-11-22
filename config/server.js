7d7c76430bf9186d84dce55c0502fe1b397fe5b9
tamesisblade <hitlerblade123456@gmail.com>
2021-11-22 09:22:38 -0500

    primero

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'efad33b3693b7780daa7e6d3a9cc5d97'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '042e3c1a-ecb8-49ea-9a2d-790ffd95b466'
+};
